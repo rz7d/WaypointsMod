4361060a783bdee7e6df2cc2ae17858218c5dd93
Fyu <cinquepvp@gmail.com>
2016-02-26 13:59:56 +0100

    Replace colons in the server ip with a hyphen to prevent issues with the filename

--- src/main/java/pw/cinque/waypoints/Waypoint.java
+++ src/main/java/pw/cinque/waypoints/Waypoint.java
@@ -56,12 +56,12 @@ public class Waypoint {
 	
 	@Override
 	public String toString() {
-		return name + ";" + world + ";" + server + ";" + x + ";" + y + ";" + z + ";" + color;
+		return name + ";" + world + ";" + server.replace(":", "-") + ";" + x + ";" + y + ";" + z + ";" + color;
 	}
 	
 	public static Waypoint fromString(String string) {
 		String[] parts = string.split(";");
-		return new Waypoint(parts[0], parts[1], parts[2], Integer.valueOf(parts[3]), Integer.valueOf(parts[4]), Integer.valueOf(parts[5]), Integer.valueOf(parts[6]));
+		return new Waypoint(parts[0], parts[1], parts[2].replace("-", ":"), Integer.valueOf(parts[3]), Integer.valueOf(parts[4]), Integer.valueOf(parts[5]), Integer.valueOf(parts[6]));
 	}
 
 }
