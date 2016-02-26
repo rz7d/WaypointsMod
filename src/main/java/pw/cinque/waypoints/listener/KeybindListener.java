0bcbe68a5e15409776d1fa47b08b3cd606b52708
Fyu <cinquepvp@gmail.com>
2016-02-26 13:31:01 +0100

    Only allow the creation of waypoints in multiplayer

--- src/main/java/pw/cinque/waypoints/listener/KeybindListener.java
+++ src/main/java/pw/cinque/waypoints/listener/KeybindListener.java
@@ -1,6 +1,8 @@
 package pw.cinque.waypoints.listener;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.util.ChatComponentText;
+import net.minecraft.util.EnumChatFormatting;
 import pw.cinque.waypoints.WaypointsMod;
 import pw.cinque.waypoints.gui.screen.GuiCreateWaypoint;
 import cpw.mods.fml.common.eventhandler.SubscribeEvent;
@@ -9,14 +11,18 @@ import cpw.mods.fml.common.gameevent.InputEvent.KeyInputEvent;
 public class KeybindListener {
 
 	private Minecraft mc = Minecraft.getMinecraft();
-	
+
 	@SubscribeEvent
 	public void onKeyInput(KeyInputEvent event) {
 		if (WaypointsMod.bindWaypointCreate.isPressed()) {
-			mc.displayGuiScreen(new GuiCreateWaypoint());
+			if (mc.isSingleplayer()) {
+				mc.thePlayer.addChatMessage(new ChatComponentText(EnumChatFormatting.RED + "You can only create waypoints in multiplayer!"));
+			} else {
+				mc.displayGuiScreen(new GuiCreateWaypoint());
+			}
 		} else if (WaypointsMod.bindWaypointMenu.isPressed()) {
 			// TODO: Finish the menu screen
 		}
 	}
-	
+
 }
