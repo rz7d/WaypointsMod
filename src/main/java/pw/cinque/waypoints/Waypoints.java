dfe8a39c9eb77f4cd76eb16214ba4f5b7020cff7
Fyu <cinquepvp@gmail.com>
2016-02-26 10:51:40 +0100

    Added keybinds

--- src/main/java/pw/cinque/waypoints/Waypoints.java
+++ src/main/java/pw/cinque/waypoints/Waypoints.java
@@ -1,5 +1,23 @@
 package pw.cinque.waypoints;
 
+import org.lwjgl.input.Keyboard;
+
+import net.minecraft.client.settings.KeyBinding;
+import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(name = "Fyu's Waypoints", modid = "waypoints", version = "0.1")
 public class Waypoints {
 
+	private KeyBinding bindWaypointCreate = new KeyBinding("Create Waypoint", Keyboard.KEY_SEMICOLON, "Fyu's Waypoints");
+	private KeyBinding bindWaypointMenu = new KeyBinding("Open Menu", Keyboard.KEY_GRAVE, "Fyu's Waypoints");
+	
+	@EventHandler
+	public void init(FMLInitializationEvent event) {
+		ClientRegistry.registerKeyBinding(bindWaypointCreate);
+		ClientRegistry.registerKeyBinding(bindWaypointMenu);
+	}
+	
 }
