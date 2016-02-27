2b10457ff829933a93f784ea62e4d8f69851f1ba
Fyu <cinquepvp@gmail.com>
2016-02-27 10:57:08 +0100

    No longer replace the colon in the server ip with a hyphen

--- src/main/java/pw/cinque/waypoints/Waypoint.java
+++ src/main/java/pw/cinque/waypoints/Waypoint.java
@@ -1,11 +1,12 @@
 package pw.cinque.waypoints;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.entity.Entity;
 
 public class Waypoint {
 
 	private static Minecraft mc = Minecraft.getMinecraft();
-	
+
 	private final String name;
 	private final String world;
 	private final String server;
@@ -25,11 +26,11 @@ public class Waypoint {
 	public String getName() {
 		return name;
 	}
-	
+
 	public String getWorld() {
 		return world;
 	}
-	
+
 	public String getServer() {
 		return server;
 	}
@@ -49,19 +50,26 @@ public class Waypoint {
 	public int getColor() {
 		return color;
 	}
-	
+
 	public boolean shouldRender() {
 		return !mc.isSingleplayer() && mc.theWorld.provider.getDimensionName().equals(world) && mc.func_147104_D().serverIP.equals(server);
 	}
-	
+
+	public double getDistance(Entity en) {
+		double x = this.x - en.posX;
+		double y = this.y - en.posY;
+		double z = this.z - en.posZ;
+		return Math.sqrt(x * x + y * y + z * z);
+	}
+
 	@Override
 	public String toString() {
-		return name + ";" + world + ";" + server.replace(":", "-") + ";" + x + ";" + y + ";" + z + ";" + color;
+		return name + ";" + world + ";" + server + ";" + x + ";" + y + ";" + z + ";" + color;
 	}
-	
+
 	public static Waypoint fromString(String string) {
 		String[] parts = string.split(";");
-		return new Waypoint(parts[0], parts[1], parts[2].replace("-", ":"), Integer.valueOf(parts[3]), Integer.valueOf(parts[4]), Integer.valueOf(parts[5]), Integer.valueOf(parts[6]));
+		return new Waypoint(parts[0], parts[1], parts[2], Integer.valueOf(parts[3]), Integer.valueOf(parts[4]), Integer.valueOf(parts[5]), Integer.valueOf(parts[6]));
 	}
 
 }
